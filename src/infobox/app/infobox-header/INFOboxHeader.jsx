14b924cccb79adbef8a6793fb916d5bd87a01169
Alina Moskalenko <alina.kravchenko@ukr.net>
2018-11-13 22:29:18 +0200

    Add update infobox

--- src/infobox/app/infobox-header/INFOboxHeader.jsx
+++ src/infobox/app/infobox-header/INFOboxHeader.jsx
@@ -4,28 +4,9 @@ import './INFOboxHeader.scss';
 const IMG_URL = './assets/images/';
 
 export class INFOboxHeader extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            images: [
-                {
-                    url: '../../../../assets/images/comp_plate_promo1.png',
-                },
-                {
-                    url: '../../../../assets/images/comp_plate_promo2.png',
-                },
-                {
-                    url: '../../../../assets/images/comp_plate_promo3.png',
-                },
-                {
-                    url: '../../../../assets/images/comp_plate_promo4.png',
-                },
-            ],
-        }
-    }
     render() {
         const contentIMG = this.props.infobox_contents.map((content, i) => {
-            // const img = IMG_URL + content.img;
+            const img = IMG_URL + content.img;
 
             let classNames = 'header__image ';
 
@@ -34,12 +15,12 @@ export class INFOboxHeader extends React.Component {
             } else if (i !== this.props.id) {
                 classNames += 'header__image_hidden';
             }
-            console.log(this.state.images[i].url);
+
             return <img key={i}
                 className={classNames}
-                src={this.state.images[i].url}
-                // src={img}
-                alt="Donats" />
+                src={img}
+                // src ={require('../../../../assets/images/infobox/comp_plate_promo1.png')}
+                alt="Image post" />
         });
 
         return <div className="infobox__header">
